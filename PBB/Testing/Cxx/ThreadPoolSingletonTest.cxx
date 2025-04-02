b1082f14b5096a67b0229002d9d0abf403ecf9b9
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2025-04-02 18:10:14 +0200

    Forgot singleton test

---
+++ PBB/Testing/Cxx/ThreadPoolSingletonTest.cxx
@@ -0,0 +1,21 @@
+#include <catch2/catch_test_macros.hpp>
+
+#include <PBB/ThreadPoolSingleton.h>
+
+#include <iostream>
+#include <mutex>
+#include <sstream>
+#include <string>
+
+namespace
+{
+
+} // namespace
+
+TEST_CASE("InstantiateAndUse", "[ThreadPoolSingleton]")
+{
+    auto& Instance = PBB::GetDefaultThreadPool();
+    auto future = Instance.Submit([=]() -> void { /* Do nothing */ }, nullptr);
+    future.Get();
+    REQUIRE(true);
+}
